0831877bf6d75fab5e1062bb042b3b8bd8ef906f
Vandice Tech <amahlephumla80@gmail.com>
2026-07-16 12:44:09 +0000

    commit

---
+++ unit3_practical.py
@@ -0,0 +1,11 @@
+#calculator.py
+
+#Get two numbers from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+#Display heading
+print("\n======== Calculator Results ========")
+
+#Addition
+print(f"Subtraction    : {round(num1 + num2)}")
