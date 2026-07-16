8bef184e13ae89f2753346da29a0953526b696a6
Vandice Tech <amahlephumla80@gmail.com>
2026-07-16 14:01:02 +0000

    commit

--- unit3_practical.py
+++ unit3_practical.py
@@ -9,3 +9,19 @@ print("\n======== Calculator Results ========")
 
 #Addition
 print(f"Subtraction    : {round(num1 + num2)}")
+
+#Subtraction
+print(f"Subtraction    : {round(num1 - num2)}")
+
+#Multiplication
+print(f"Multiplication : {round(num1 * num2)}")
+
+# Check for division by zero
+if num2 != 0:
+   print(f"Divison   : {round(num1 / num2, 2)}")
+   print(f"Floor Division   : {round(num1 // num2, 2)}")
+   print(f"Modulus    : {round(num1 % num2, 2)}")
+else:
+   print("Division     : Cannot divide by zero.")
+   print("Floor Division :Cannot divide by zero.")
+   print("Modulus        : Cannot divide by zero.")
