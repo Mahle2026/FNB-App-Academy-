5dcce841885c9e8ff0926a4385845a9800ccd3e3
Vandice Tech <amahlephumla80@gmail.com>
2026-07-16 12:01:40 +0000

    commit

--- unit1_challenge.py
+++ unit1_challenge.py
@@ -1,3 +1,10 @@
+#The Concert Ticket Booker
+
+#Ask the user for their name
 name = input("Enter your name: ")
+
+#Ask for the band or artist
 artist = input("Enter the name of the artist you want to see: ")
+
+#Display the confirmation message
 print(f"Hey {name}! Your ticket to see {artist} are booked successfuly!")
