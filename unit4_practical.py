29bdd3dde55cc7c8020fe0909eabf23d04840dc3
Amahle Matandela <amahlephumla80@gmail.com>
2026-07-17 12:32:26 +0200

    update unit4_practical.py

--- unit4_practical.py
+++ unit4_practical.py
@@ -1,45 +1,45 @@
-# grade_classfier.py  
+# grade_classifier.py
 
-#Collect learners details
+# Collect learner details
 name = input("Enter learner name: ")
 mark1 = float(input("Enter mark for Subject 1: "))
 mark2 = float(input("Enter mark for Subject 2: "))
 mark3 = float(input("Enter mark for Subject 3: "))
 
-#Calculate average
-average = (mark1 + mark2 + mark3) / 3 
+# Calculate average
+average = (mark1 + mark2 + mark3) / 3
 
-#Assign grade
+# Assign grade
 if average >= 80:
-  grade = "A
+    grade = "A"
 elif average >= 70:
-  grade = "B"
+    grade = "B"
 elif average >= 60:
-  grade = "C"
+    grade = "C"
 elif average >= 50:
-  grade = "D"
+    grade = "D"
 else:
-  grade = "F"
-  
-#Assign pass/fail status
-if average >=50:
-  status = "Pass"
-else:
-  status = "Fail"
+    grade = "F"
 
-#Check for intervention
-intervention = []
+# Assign pass/fail status
+if average >= 50:
+    status = "Pass"
+else:
+    status = "Fail"
+
+# Check for intervention
+interventions = []
 
 if mark1 < 40:
-  intervention.append("Subject 1 needs intervention")
+    interventions.append("Subject 1 needs intervention")
 if mark2 < 40:
-  intervention.append("Subject 2 needs intervention")
+    interventions.append("Subject 2 needs intervention")
 if mark3 < 40:
-  intervention.append("Subject 3 needs intervention")
-  
-  #Display report card
+    interventions.append("Subject 3 needs intervention")
+
+# Display report card
 print("\n========== REPORT CARD ==========")
-print(f"Learners Name: {name}")
+print(f"Learner Name: {name}")
 print(f"Subject 1 Mark: {mark1}")
 print(f"Subject 2 Mark: {mark2}")
 print(f"Subject 3 Mark: {mark3}")
@@ -48,10 +48,10 @@ print(f"Grade: {grade}")
 print(f"Status: {status}")
 
 if interventions:
-  print("Intervention Flags): ")
-  for item in interventions:
-    print(f"- {item}")
-  else:
+    print("Intervention Flags:")
+    for item in interventions:
+        print(f"- {item}")
+else:
     print("Intervention Flags: None")
-    
-  print("=================================")
+
+print("=================================")
